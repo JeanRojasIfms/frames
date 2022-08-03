51b2d266f3924be64b558b8b3af3acea34cdad59
JeanRojasIfms <jean.rojas@ifms.edu.br>
2022-08-03 08:18:31 -0400

    Projeto Detran - Classes de modelo criadas

---
+++ src/main/java/br/edu/ifms/frames/FramesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.frames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FramesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FramesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifms/frames/FramesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.frames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FramesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
